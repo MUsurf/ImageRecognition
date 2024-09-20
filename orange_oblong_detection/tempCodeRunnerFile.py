b7c973ed9400a45e1b7f8424a2b5c08c8d126d1a
PhantomOffKanagawa <harry@surmafamily.com>
2024-09-19 19:52:04 -0500

    Added comments

--- orange_oblong_detection/tempCodeRunnerFile.py
+++ orange_oblong_detection/tempCodeRunnerFile.py
@@ -3,7 +3,6 @@ import numpy as np
 import math
 camera_index = 0
 
-# If running in test environment then import helper
 if __debug__:
     from classes.helper import *
 
@@ -56,44 +55,40 @@ while True:
     
     
     
-    # If there are any detected contours
+
     if len(contours) > 0:
         # Get the largest contour found as this is likely the wanted shape
         largest_contour = max(contours, key=cv.contourArea)
         
+        
+        
         try:
-            # Get the convex hull of a point set to detect angle point
             hull = cv.convexHull(largest_contour,returnPoints = False)
-            # Get all the examples of convexity
             defects = cv.convexityDefects(largest_contour,hull)
             
-            # We want to keep the furthest point so track params
-            maxDefect = [0,0,0,0]
+            maxF = 0
+            maxE = 0
+            maxD = 0
+            maxS = 0
             
-            # If there are defects
             if(defects is not None):
-                # For all defects (convex points)
                 for i in range(defects.shape[0]):
-                    # Get relevant points [start, end, far, distance]
-                    # start: index (of start of line of convexity),
-                    # end: index (of end of line with convexity),
-                    # far: convex point,
-                    # distance: distance of far from line of convexity
-                    test = defects[i,0]
-
-                    # If distance is greater than current max
-                    if(test[3] > maxDefect[3]):
-                        ## far = tuple(largest_contour[test[2]][0])
-                        # Update max to new largest
-                        maxDefect = test 
+                    s,e,f,d = defects[i,0]
+                    
+                    if(d > maxD):
+                        far = tuple(largest_contour[f][0])
+                        maxS = s
+                        maxE = e
+                        maxD = d
+                        maxF = f
                 
                 
-                # Get the relevant points for the start, end, and far point
-                start = tuple(largest_contour[maxDefect[0]][0])
-                end = tuple(largest_contour[maxDefect[1]][0])
-                far = tuple(largest_contour[maxDefect[2]][0])
+                    
+                start = tuple(largest_contour[maxS][0])
+                end = tuple(largest_contour[maxE][0])
+                far = tuple(largest_contour[maxF][0])
                 
-                ## Code from https://gist.github.com/Dhruv454000/dce6491280e09ff8d920ed46fc625889
+                
                 # find length of all sides of triangle
                 a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
                 b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
@@ -105,25 +100,42 @@ while True:
                 # apply cosine rule here
                 angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
             
-                # Draw the two lines of the convexity to the convex point
                 cv.line(frame,start,far,[0,255,0],2)
                 cv.line(frame,far,end,[0,255,0],2)
-                # Draw the point of the angle
+                
+                
+
                 cv.circle(frame,far,5,[0,0,255],-1)
-        # If error from opencv then skip this frame
+                
+                    
+                    
+                
         except cv.error:
             continue
-
-    # If running in a test environment show the results visually
-    if __debug__:            
-        cv.imshow('img', frame)
-        cv.imshow('mask', mask_smoothed)
+            
+    cv.imshow('img', frame)
+    cv.imshow('mask', mask_smoothed)
+    
+    if __debug__:
         combined = blend_frame_with_mask(frame, mask_smoothed)
         cv.imshow('combined', combined)
     
-    # Wait for q key to quit
+
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 cv.destroyAllWindows()
 cap.release()
+
+
+# img_gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+# ret,thresh = cv.threshold(img_gray, 127, 255,0)
+# contours,hierarchy = cv.findContours(thresh,2,1)
+# cnt = contours[0]
+# print(cnt)
+
+
+
+
+#print(defects)
+
