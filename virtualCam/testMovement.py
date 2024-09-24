55223ace166cb1624bc80b4b0107cae28a9be26b
PhantomOffKanagawa <harry@surmafamily.com>
2024-09-24 11:57:10 -0500

    Added Virtual Sub Camera

--- virtualCam/testMovement.py
+++ virtualCam/testMovement.py
@@ -1,14 +1,19 @@
 import random
 from vCam import *
 
-# CHANGE exampleMovement to your file
+## CHANGE exampleMovement to your file
 from exampleMovement import *
 
 
 def main():
+    # Generate a random position
     pos = [random.randint(-3, 3), random.randint(-3, 3), random.randint(1, 5)]
-    rotation = 0                              # Rotation in degrees
-    # rotation = random.randint(0, 360)           # Random Rotation in degrees (More Difficult)
+    
+    # Generate a 0 rotation or a random rotation
+    rotation = 0                                  # Rotation in degrees
+    ## rotation = random.randint(0, 360)           # Random Rotation in degrees (More Difficult)
+
+    # Set the new position
     set_pos(pos, rotation)
     
     running = True
@@ -26,14 +31,14 @@ def main():
         handle_keyboard_input()
 
         # User movement controls
-        # passed (vx, vy, x, y)
+        # passed (vx, vy, x, y), width, height, camera_rotation
         tick(lineValues, WIDTH, HEIGHT, camera_rotation)
 
         # Render the scene
         render_scene()
 
-
         # Limit the frame rate to 30 FPS
+        # Change for faster or slower
         clock.tick(30)
 
     pygame.quit()
