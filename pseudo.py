dc6e849b06f593f0c48a296418526e843f4d7979
PhantomOffKanagawa <harry@surmafamily.com>
2024-10-22 19:03:09 -0500

    Added pseudo-code for popsicle cameras

---
+++ pseudo.py
@@ -0,0 +1,12 @@
+## Front Camera
+# Center Horizontal & Go Forward
+# Stop When Bottom, See
+
+## Bottom Camera
+# Center Orange X, Y
+# Z Height Comment
+# Center Rotation
+# Stop
+
+## Back
+# Go Forward & Center Horizontally
