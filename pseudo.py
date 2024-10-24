9016ea4fc315dfa4309e70493d0be0f4a34da6bb
PhantomOffKanagawa <harry@surmafamily.com>
2024-10-24 18:54:43 -0500

    Pseudo Code

--- pseudo.py
+++ pseudo.py
@@ -6,6 +6,18 @@ def soft_right_turn():
     pass
 def full_forward():
     pass
+def move_left():
+    pass
+def move_right():
+    pass
+def move_up():
+    pass
+def move_down():
+    pass
+def turn_left():
+    pass
+def turn_right():
+    pass
 
 ## Constant for screen length & height
 SCREEN_LENGTH = 100
@@ -45,16 +57,37 @@ while centered[0] and centered[1]:
     horizontal_middle = (SCREEN_LENGTH * (0.5 - HORIZONTAL_THRESH))
 
     if midpoint_x > horizontal_middle:
-        soft_left_turn()
+        move_left()
         centered[0] = False
     elif midpoint_x < horizontal_middle:
-        soft_right_turn()
+        move_right()
         centered[0] = False
     else:
         centered[0] = True
 
+    vertical_middle = (SCREEN_HEIGHT * (0.5 - VERTICAL_THRESH))
+    if midpoint_y > vertical_middle:
+        move_up()
+        centered[1] = False
+    elif midpoint_x < vertical_middle:
+        move_down()
+        centered[1] = False
+    else:
+        centered[1] = True
+
 # Z Height Comment
 # Center Rotation
+while abs(angle - 90):
+    horizontal_middle = (SCREEN_LENGTH * (0.5 - HORIZONTAL_THRESH))
+
+    if midpoint_x > horizontal_middle:
+        move_left()
+        centered[0] = False
+    elif midpoint_x < horizontal_middle:
+        move_right()
+        centered[0] = False
+    else:
+        centered[0] = True
 # Stop
 
 ## Back
